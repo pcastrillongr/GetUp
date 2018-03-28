44ca59f85206c28e410714999700a2b5023c3173
pablocastrillon <cristinavilas@192.168.1.33>
2018-03-28 13:16:22 +0200

    Primer Layout

--- mobile/src/main/java/com/example/cristinavilas/getup/Entrada_Juego.java
+++ mobile/src/main/java/com/example/cristinavilas/getup/Entrada_Juego.java
@@ -1,5 +1,6 @@
 package com.example.cristinavilas.getup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,13 @@ public class Entrada_Juego extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entrada__juego);
+        try {
+            Thread.sleep(400);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Intent go=new Intent(this,Menu_Alarma.class);
+
+
     }
 }
---
+++ mobile/src/main/java/com/example/cristinavilas/getup/Menu_Alarma.java
@@ -0,0 +1,13 @@
+package com.example.cristinavilas.getup;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Menu_Alarma extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu__alarma);
+    }
+}
