27c3f7e464d6b0c4a043c76c27170087bbe258f3
LAPTOP-3D4TSPFH\aleja <alevillalobos.1993@gmail.com>
2018-03-31 00:01:26 +0200

    Mejora diseño CrearAlarma

--- mobile/src/main/java/com/example/cristinavilas/getup/CrearAlarma/CrearAlarma.java
+++ mobile/src/main/java/com/example/cristinavilas/getup/CrearAlarma/CrearAlarma.java
@@ -1,19 +1,16 @@
 package com.example.cristinavilas.getup.CrearAlarma;
 
+
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.TimePicker;
+
 
 import com.example.cristinavilas.getup.R;
 
@@ -22,6 +19,8 @@ public class CrearAlarma extends AppCompatActivity {
 
     private SeleccionarFragment seleccionarFragment;
     private ViewPager mViewPager;
+    private TimePicker simpleTimePicker;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +32,7 @@ public class CrearAlarma extends AppCompatActivity {
         // primary sections of the activity.
         seleccionarFragment = new SeleccionarFragment(getSupportFragmentManager());
 
+
         // Set up the ViewPager with the sections adapter.
         mViewPager = findViewById(R.id.container);
         mViewPager.setAdapter(seleccionarFragment);
@@ -42,13 +42,7 @@ public class CrearAlarma extends AppCompatActivity {
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
 
-        FloatingActionButton Next = findViewById(R.id.Next);
-        Next.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-            }
-        });
+
 
     }
 
