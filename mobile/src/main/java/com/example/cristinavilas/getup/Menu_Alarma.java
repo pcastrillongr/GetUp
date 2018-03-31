0ce1cfeff9c36c5e93b2bce03887c81d7f2b9e78
pablocastrillon <cristinavilas@192.168.1.33>
2018-03-31 03:14:14 +0200

    Menu Alarma direcciona a crear alarma

--- mobile/src/main/java/com/example/cristinavilas/getup/Menu_Alarma.java
+++ mobile/src/main/java/com/example/cristinavilas/getup/Menu_Alarma.java
@@ -1,6 +1,7 @@
 package com.example.cristinavilas.getup;
 
 import android.content.Intent;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,13 +13,18 @@ import com.example.cristinavilas.getup.CrearAlarma.CrearAlarma;
 public class Menu_Alarma extends AppCompatActivity {
 
     ImageView  anhadiralarma;
+    ImageView  menupreferencias;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu__alarma);
 
+
         anhadiralarma=(ImageView) findViewById(R.id.anhadir);
+        menupreferencias=(ImageView)findViewById(R.id.menu);
         anhadiralarma.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View view) {
 
@@ -27,5 +33,17 @@ public class Menu_Alarma extends AppCompatActivity {
 
             }
         });
+        menupreferencias.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent go=new Intent(getApplicationContext(), Preferencias_Alarma.class);
+                startActivity(go);
+
+            }
+        });
+
     }
+
+
 }
